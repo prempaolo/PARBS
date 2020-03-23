5692aa3c52c68f4436530328689f6088724619d8
prempaolo <prempaolo@gmail.com>
2020-03-23 16:15:45 +0100

    Fixes UEFI support

--- arch_install.sh
+++ arch_install.sh
@@ -10,10 +10,6 @@ ping -q -c 1 -W 1 8.8.8.8 &>/dev/null || { echo "Setup an internet connection be
 # Check if system is UEFI or BIOS
 ls /sys/firmware/efi/efivars &>/dev/null && UEFI=true
 
-echo $UEFI
-
-sleep 2
-
 timedatectl set-ntp true
 
 COUNTER=1
@@ -34,25 +30,25 @@ done
 PARTITION="$(fdisk -l | grep 'Disk /dev' | sed "${CHOICE}q;d" | awk -F " " '{ print substr($2, 1, length($2)-1) }')"
 
 # Creates three partitions, /, /home and swap
-if [ $UEFI ]; then
+if [ $UEFI = true ]; then
 	(echo n; echo p; echo 1; echo ""; echo "+260M"; echo t; echo ef; echo n; echo p; echo 2; echo ""; echo "$ROOT_PARTITION_SPACE"; echo n; echo p; echo 3; echo ""; echo "$SWAP_PARTITION_SPACE"; echo n; echo p; echo ""; echo ""; echo t; echo 3; echo 82; echo w) | fdisk "$PARTITION";
 else
 	(echo n; echo p; echo 1; echo ""; echo "$ROOT_PARTITION_SPACE"; echo n; echo p; echo 2; echo ""; echo "$SWAP_PARTITION_SPACE"; echo n; echo p; echo 3; echo ""; echo ""; echo t; echo 2; echo 82; echo w) | fdisk "$PARTITION";
 fi
 
-[ $UEFI ] && mkfs.fat -F32 "$PARTITION"1
-if [ $UEFI ]; then mkfs.ext4 "$PARTITION"2; else mkfs.ext4 "$PARTITION"1; fi
-if [ $UEFI ]; then mkswap "$PARTITION"3; swapon "$PARTITION"3; else mkswap "$PARTITION"2; swapon "$PARTITION"2; fi
-if [ $UEFI ]; then mkfs.ext4 "$PARTITION"4; else mkfs.ext4 "$PARTITION"3; fi
-if [ $UEFI ]; then mount "$PARTITION"2 /mnt; else mount "$PARTITION"1 /mnt; fi
+[ $UEFI = true ] && mkfs.fat -F32 "$PARTITION"1
+if [ $UEFI = true ]; then mkfs.ext4 "$PARTITION"2; else mkfs.ext4 "$PARTITION"1; fi
+if [ $UEFI = true ]; then mkswap "$PARTITION"3; swapon "$PARTITION"3; else mkswap "$PARTITION"2; swapon "$PARTITION"2; fi
+if [ $UEFI = true ]; then mkfs.ext4 "$PARTITION"4; else mkfs.ext4 "$PARTITION"3; fi
+if [ $UEFI = true ]; then mount "$PARTITION"2 /mnt; else mount "$PARTITION"1 /mnt; fi
 mkdir /mnt/home
-if [ $UEFI ]; then mount "$PARTITION"4 /mnt/home; else mount "$PARTITION"3 /mnt/home; fi
-[ $UEFI ] && { mkdir /mnt/efi; mount "$PARTITION"1 /mnt/efi; }
+if [ $UEFI = true ]; then mount "$PARTITION"4 /mnt/home; else mount "$PARTITION"3 /mnt/home; fi
+[ $UEFI = true ] && { mkdir /mnt/efi; mount "$PARTITION"1 /mnt/efi; }
 
 pacstrap /mnt base linux linux-firmware vim
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
 # Commands to execute as chroot
-if [ $UEFI ]; then cp uefi_chroot_cmds.sh /mnt/root/; else cp bios_chroot_cmds.sh /mnt/root; fi
-if [ $UEFI ]; then arch-chroot /mnt /root/uefi_chroot_cmds.sh; else arch-chroot /mnt /root/bios_chroot_cmds.sh; fi
+if [ $UEFI = true ]; then cp uefi_chroot_cmds.sh /mnt/root/; else cp bios_chroot_cmds.sh /mnt/root; fi
+if [ $UEFI = true ]; then arch-chroot /mnt /root/uefi_chroot_cmds.sh; else arch-chroot /mnt /root/bios_chroot_cmds.sh; fi
