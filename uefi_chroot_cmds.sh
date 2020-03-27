49ab8c4db2c66392db5453dd4464bd33ab71fda6
prempaolo <prempaolo@gmail.com>
2020-03-27 19:03:25 +0100

    Updates

--- uefi_chroot_cmds.sh
+++ uefi_chroot_cmds.sh
@@ -6,6 +6,8 @@ sed -i '/#en_US.UTF-8 UTF-8/s/^#//g' /etc/locale.gen
 
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
+locale-gen
+
 echo "archlinux" > /etc/hostname
 
 echo -e "127.0.0.1\tlocalhost\n::1\t\tlocalhost\n127.0.1.1\tarchlinux.local\tarchlinux" >> /etch/hosts
